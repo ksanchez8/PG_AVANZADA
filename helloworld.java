3e9c9bcf5335895b847b7df8df01cf527ba47251
ksanchez8 <ksanchez@institutmvm.cat>
2024-04-16 10:52:10 +0200

    Creación de HelloWorld

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+class HelloWorld
+{
+    /**
+     * Una funcion se declara poniendo:
+     * Su visibilidad
+     * Si es static o no (si no es static no se pone nada)
+     * El tipo de dato que retorna, si no devuelve nada es void
+     * Nombre del metodo
+     */
+
+    public static void main()
+
+
+}
+
