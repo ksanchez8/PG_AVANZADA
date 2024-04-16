25c43d4ed2f1e3ad68e7340f5c2547c6fc3eca92
ksanchez8 <ksanchez@institutmvm.cat>
2024-04-16 10:56:35 +0200

    Pintln

--- helloworld.java
+++ helloworld.java
@@ -6,9 +6,12 @@ class HelloWorld
      * Si es static o no (si no es static no se pone nada)
      * El tipo de dato que retorna, si no devuelve nada es void
      * Nombre del metodo
+     * Parametros del metodo
      */
 
-    public static void main()
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 
 
 }
