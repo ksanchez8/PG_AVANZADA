7dbe1632b39d3f7ed09449e52d0f492eb757a1f2
ksanchez8 <ksanchez@institutmvm.cat>
2024-04-25 09:19:24 +0200

    Mètode principal que crea instàncies de la classe ClasseA amb diversos constructors, i imprimeix els valors primaris i secundaris de cada instància.

--- calculadora/instancacio_de_classes/instancacio_de_clases.java
+++ calculadora/instancacio_de_classes/instancacio_de_clases.java
@@ -47,6 +47,15 @@ public class ClasseA {
         return this.valorSecundari;
     }
 
+    public static void main(String[] args) {
+        ClasseA a = new ClasseA();
+        ClasseA b = new ClasseA(20);
+        ClasseA c = new ClasseA(20, 40);
+        System.out.println("L'objecte _a:_ conté: " + a.getPrimari() + ", " + a.getSecundari());
+        System.out.println("L'objecte _b:_ conté: " + b.getPrimari() + ", " + b.getSecundari());
+        System.out.println("L'objecte _c:_ conté: " + c.getPrimari() + ", " + c.getSecundari());
+    }
+
 
 }
 
