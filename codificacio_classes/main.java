c7453363efe2afb8b5874467c5cfd96fdc8290a7
ksanchez8 <ksanchez@institutmvm.cat>
2024-04-25 09:49:15 +0200

    Asignación de valores a los atributos utilizando los métodos setters

--- codificacio_classes/main.java
+++ codificacio_classes/main.java
@@ -3,3 +3,7 @@ import media.media;
 public class Main {
     public static void main(String[] args) {
         Media mitjana = new Media();
+
+        mitjana.setNom("Títol de la Mitjana");
+        mitjana.setAutor("Nom de l'Autor");
+        mitjana.setDurada(120);
