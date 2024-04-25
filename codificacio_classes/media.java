8d75c414983717705237de63faa215a653510ce2
ksanchez8 <ksanchez@institutmvm.cat>
2024-04-25 09:42:29 +0200

    Constructor con parámetros para inicializar los atributos y correción de tabulación

--- codificacio_classes/media.java
+++ codificacio_classes/media.java
@@ -5,5 +5,11 @@ public class Media {
     private String autor;
     private int duradaSegons;
 
-public Media() {
+    public Media() {
+    }
+
+    public Media(String nom, String autor, int duradaSegons) {
+        this.nom = nom;
+        this.autor = autor;
+        this.duradaSegons = duradaSegons;
     }
