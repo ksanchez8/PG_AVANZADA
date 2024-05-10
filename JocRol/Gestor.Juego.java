eddba0ed48f2f6113463649a8d1b2607b48a0d97
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-10 08:52:41 +0200

    Lista para almacenar los personajes cargados desde el XML

--- JocRol/Gestor.Juego.java
+++ JocRol/Gestor.Juego.java
@@ -5,3 +5,10 @@ import org.w3c.dom.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+public class GestorJuego {
+      public static List<Personaje> cargarPersonajesDesdeXML(String rutaArchivo) {
+        List<Personaje> personajes = new ArrayList<>();
+
+
+}
