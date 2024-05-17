432fd7eee4a3ccb024c2a927e458f5d7c8c546ca
ksanchez8 <ksanchez@institutmvm.cat>
2024-05-17 09:46:20 +0200

    Creación de import Scanner

--- GestorDeTasques/TaskManagement.java
+++ GestorDeTasques/TaskManagement.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 class TaskManagement {
     private HashMap<String, ArrayList<Task>> lists;
