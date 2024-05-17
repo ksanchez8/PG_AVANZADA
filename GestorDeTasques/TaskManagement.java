0c7b2fb06f1df3674e2225da2f646617e02e991d
ksanchez8 <ksanchez@institutmvm.cat>
2024-05-17 08:49:20 +0200

    Creación de la clase TaskManagement

--- GestorDeTasques/TaskManagement.java
+++ GestorDeTasques/TaskManagement.java
@@ -6,3 +6,15 @@ import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+
+class TaskManagement {
+    private HashMap<String, ArrayList<Task>> lists;
+    private HashMap<Integer, Task> tasks;
+    private String filename;
+
+    public TaskManagement(String filename) {
+        this.filename = filename;
+        lists = new HashMap<>();
+        tasks = new HashMap<>();
+        loadTasksFromFile(); // Cargamos las tareas del archivo al iniciar el programa
+    }
