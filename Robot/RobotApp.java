feadca447840e541628150759d6cafbf7c8ccd82
ksanchez8 <ksanchez@institutmvm.cat>
2024-05-24 09:04:06 +0200

    Crear una nueva ventana JFrame para la aplicación

--- Robot/RobotApp.java
+++ Robot/RobotApp.java
@@ -4,10 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RobotApp {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Control de Robot");
 
 
 
 
 
-    
+
 }
