c5d9f9f5db8b84282b28b7839004696fcde37659
ksanchez8 <ksanchez@institutmvm.cat>
2024-05-24 09:10:08 +0200

    Correción de tabulación

--- Robot/RobotApp.java
+++ Robot/RobotApp.java
@@ -6,22 +6,24 @@ import java.awt.event.ActionListener;
 public class RobotApp {
     public static void main(String[] args) {
 
+
         JFrame frame = new JFrame("Control de Robot");
 
         JPanel controlPanel = new JPanel();
 
-       JButton moveForwardButton = new JButton("Moure Endavant");
-       JButton moveBackwardButton = new JButton("Moure Enrere");
-       JButton rotateLeftButton = new JButton("Girar Esquerra");
-       JButton rotateRightButton = new JButton("Girar Dreta");
-       JButton saveStateButton = new JButton("Guardar Estat");
-       JButton loadStateButton = new JButton("Carregar Estat");
 
-       JTextArea consoleOutput = new JTextArea(10, 20);
+        JButton moveForwardButton = new JButton("Moure Endavant");
+        JButton moveBackwardButton = new JButton("Moure Enrere");
+        JButton rotateLeftButton = new JButton("Girar Esquerra");
+        JButton rotateRightButton = new JButton("Girar Dreta");
+        JButton saveStateButton = new JButton("Guardar Estat");
+        JButton loadStateButton = new JButton("Carregar Estat");
 
-       consoleOutput.setEditable(false);
+        JTextArea consoleOutput = new JTextArea(10, 20);
 
-       controlPanel.setLayout(new GridLayout(3, 2));
+        consoleOutput.setEditable(false);
+
+        controlPanel.setLayout(new GridLayout(3, 2));
 
 
 
