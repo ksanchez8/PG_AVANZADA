e15d27196452cc721a8f39f848b75cc9ee643160
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-29 19:19:20 +0200

    Correción de codigo y de tabulación

--- AppBasica/SalutacioAppBasica.java
+++ AppBasica/SalutacioAppBasica.java
@@ -6,24 +6,26 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage; 
 
 public class SalutacioAppBasica extends Application {
-    Button button = new Button("Clica aquí");
 
-    button.setOnAction(event -> {
+    public void start(Stage primaryStage) {
+        Button button = new Button("Clica aquí");
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Salutació");  // Establece el título de la alerta
-        alert.setHeaderText(null);    // No establece encabezado para la alerta
-        alert.setContentText("Hola! Gràcies per clicar el botó.");  // Establece el texto del mensaje
-        alert.showAndWait();  // Muestra la alerta y espera hasta que el usuario la cierre
-    });
+        button.setOnAction(event -> {
 
-    StackPane root = new StackPane();
-    root.getChildren().add(button);
-    Scene scene = new Scene(root, 300, 200);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Salutació");  // Establece el título de la alerta
+            alert.setHeaderText(null);    // No establece encabezado para la alerta
+            alert.setContentText("Hola! Gràcies per clicar el botó.");  // Establece el texto del mensaje
+            alert.showAndWait();  // Muestra la alerta y espera hasta que el usuario la cierre
+        });
 
-    primaryStage.setTitle("Aplicació de Salutació");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Aplicació de Salutació");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     
 }
