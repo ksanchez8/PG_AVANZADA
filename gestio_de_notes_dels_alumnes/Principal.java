8121ed765af24f6f7c2bffc3c42d5bdb99972214
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:24:14 +0200

    Obtenir les notes d'un alumne

--- gestio_de_notes_dels_alumnes/Principal.java
+++ gestio_de_notes_dels_alumnes/Principal.java
@@ -8,5 +8,13 @@ public class Principal {
         gestor.registrarNotes("Joan", new double[] { 8.5, 7.2, 9.0 });
         gestor.registrarNotes("Maria", new double[] { 6.0, 5.5, 8.0 });
         gestor.registrarNotes("Pere", new double[] { 9.5, 8.5, 7.0 });
+    
+        double[] notesJoan = gestor.obtenirNotes("Joan");
+        System.out.println("Notes de Joan:");
+        for (double nota : notesJoan) {
+            System.out.print(nota + " ");
+        }
+        System.out.println();
     }
+
 }
