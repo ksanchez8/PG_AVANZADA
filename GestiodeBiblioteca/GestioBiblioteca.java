83ab6736cf941550aef874652d7abf97c6f0476f
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:32:03 +0200

    URL de la base de dades, usuari i contrasenya

---
+++ GestiodeBiblioteca/GestioBiblioteca.java
@@ -0,0 +1,7 @@
+package GestiodeBiblioteca;
+
+public class GestioBiblioteca {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String USER = "root";
+    private static final String PASS = "password";
+}
