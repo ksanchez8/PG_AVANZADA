aef816d8e9054c81ad9b981ed2fb98e0337f8cbf
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:05:44 +0200

    Constructor per inicialitzar els atributs, incloent la data de caducitat

--- Amazon/Aliment.java
+++ Amazon/Aliment.java
@@ -5,5 +5,10 @@ import java.time.LocalDate;
 class Aliment extends Producte {
     private LocalDate dataCaducitat;
 
+    public Aliment(int codi, String nom, double preu, LocalDate dataCaducitat) {
+        super(codi, nom, preu);
+        this.dataCaducitat = dataCaducitat;
+    }
+
 
 }
