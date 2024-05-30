b132cee9aba8fb8c5aaf5580ce48ae101f75323a
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 12:28:47 +0200

    Llegeix la ruta de la carpeta des de la consola

--- DirectoryCleaner/DirectoryCleaner.java
+++ DirectoryCleaner/DirectoryCleaner.java
@@ -4,3 +4,6 @@ import java.util.Scanner;
 public class DirectoryCleaner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introdueix la ruta de la carpeta:");
+        String directoryPath = scanner.nextLine();
