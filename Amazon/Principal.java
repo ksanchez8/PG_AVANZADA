6b9c6de9cbe0594569b254bf50a6ff6cd643186d
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:12:48 +0200

    Crear diferents tipus de productes i afegir-los a la botiga

--- Amazon/Principal.java
+++ Amazon/Principal.java
@@ -7,3 +7,8 @@ public class Principal {
     public static void main(String[] args) {
         Botiga botiga = new Botiga();
 
+        Aliment aliment = new Aliment(1, "Pa", 1.0, LocalDate.now().plusDays(5));
+        Electrodomestic electrodomestic = new Electrodomestic(2, "Nevera", 500.0, 36);
+        Llibre llibre = new Llibre(3, "El Senyor dels Anells", 30.0, "J.R.R. Tolkien", 1200);
+
+
