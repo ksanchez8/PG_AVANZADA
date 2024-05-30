7e9dcf986b34292aa3113b93e708bddcaec614e5
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:23:52 +0200

    Registrar les notes d'alumnes per a diverses assignatures

--- gestio_de_notes_dels_alumnes/Principal.java
+++ gestio_de_notes_dels_alumnes/Principal.java
@@ -4,5 +4,9 @@ public class Principal {
     public static void main(String[] args) {
 
         GestorNotes gestor = new GestorNotes();
+
+        gestor.registrarNotes("Joan", new double[] { 8.5, 7.2, 9.0 });
+        gestor.registrarNotes("Maria", new double[] { 6.0, 5.5, 8.0 });
+        gestor.registrarNotes("Pere", new double[] { 9.5, 8.5, 7.0 });
     }
 }
