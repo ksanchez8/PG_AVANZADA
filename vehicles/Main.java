e0a9e83b32109ad23950b1a2d0477a15486c5f73
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 13:29:58 +0200

    Pruebas

--- vehicles/Main.java
+++ vehicles/Main.java
@@ -4,5 +4,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        // Crear una instancia de Automobile con el modelo "Toyota Corolla"
+        Automobile car = new Automobile("Toyota Corolla");
+        
+        // Crear una instancia de Bicycle con el tipo "Mountain"
+        Bicycle bike = new Bicycle("Mountain");
 }
