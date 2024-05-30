2c0e2ca1b23ac29d673d9f63c16e1544fc891d7e
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 13:39:01 +0200

    Classe Rectangle que hereta de Figura2D i implementa els mètodes calcularArea() i calcularPerimetre().

---
+++ figures/Rectangle.java
@@ -0,0 +1,22 @@
+package figures;
+
+public class Rectangle {
+    public class Rectangle extends Figura2D {
+        private double amplada;
+        private double altura;
+    
+        public Rectangle(String nom, double amplada, double altura) {
+            super(nom);
+            this.amplada = amplada;
+            this.altura = altura;
+        }
+        
+        public double calcularArea() {
+            return amplada * altura;
+        }
+    
+        public double calcularPerimetre() {
+            return 2 * (amplada + altura);
+        }
+    }
+}
