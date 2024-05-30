cb7696d294662b60b39c8ec6b112817f8d9c3e04
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:08:48 +0200

    Constructor per inicialitzar els atributs, incloent l'autor i el nombre de pàgines

--- Amazon/Llibre.java
+++ Amazon/Llibre.java
@@ -3,4 +3,12 @@ package Amazon;
 class Llibre extends Producte {
     private String autor;
     private int numPagines;  
+
+    public Llibre(int codi, String nom, double preu, String autor, int numPagines) {
+        super(codi, nom, preu);
+        this.autor = autor;
+        this.numPagines = numPagines;
+    }
+
+
 }
