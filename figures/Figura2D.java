ab89a550e97c376852e667dfc4c2fdba5379d5bc
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 13:37:30 +0200

    Classe abstracta Figura2D que implementa la interfície FiguraGeometrica.

---
+++ figures/Figura2D.java
@@ -0,0 +1,13 @@
+package figures;
+
+public class Figura2D {
+    protected String nom;
+
+    public Figura2D(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+}
