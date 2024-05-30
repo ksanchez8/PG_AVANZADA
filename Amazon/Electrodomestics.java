954b80ee4ab7ad5ead7e9ba1b841eccac0c6742f
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 23:07:26 +0200

    Constructor per inicialitzar els atributs, incloent la garantia en mesos

--- Amazon/Electrodomestics.java
+++ Amazon/Electrodomestics.java
@@ -3,5 +3,10 @@ package Amazon;
 class Electrodomestic extends Producte {
     private int mesosGarantia;
 
+    public Electrodomestic(int codi, String nom, double preu, int mesosGarantia) {
+        super(codi, nom, preu);
+        this.mesosGarantia = mesosGarantia;
+    }
+
 
 }
