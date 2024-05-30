4dfa39f1a49d186538ce5a513e9fd41a9ae1b975
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 12:51:33 +0200

    Se crea el bucle y se muestran las opciones del programa

--- Gestioempleats/Main.java
+++ Gestioempleats/Main.java
@@ -4,5 +4,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        GestionEmpleados gestio = new GestionEmpleados();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Tria una opció:");
+            System.out.println("1. Afegir nou empleat");
+            System.out.println("2. Buscar empleat per nom");
+            System.out.println("3. Llistar tots els empleats");
+            System.out.println("4. Sortir");
+            int opcio = scanner.nextInt();
+            scanner.nextLine();
     
 }
