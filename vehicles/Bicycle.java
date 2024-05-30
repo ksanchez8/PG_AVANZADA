7db4f555d9093afcec11229bd9417fc0e0dfb5e6
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-05-30 13:24:58 +0200

    Constructor que inicializa el tipo de bicicleta y la velocidad inicial.

--- vehicles/Bicycle.java
+++ vehicles/Bicycle.java
@@ -3,4 +3,9 @@ package vehicles;
 public class Bicycle {
     private String type;
     private int speed;
+
+    public Bicycle(String type) {
+        this.type = type;
+        this.speed = 0;
+    }
 }
