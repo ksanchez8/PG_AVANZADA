7b3031a180df3bd3d51c160ca1c0c5378d2dd9aa
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-06-03 18:53:06 +0200

    Método para girar el robot a la izquierda

--- Robot2/Robot.java
+++ Robot2/Robot.java
@@ -40,4 +40,21 @@ public class Robot {
         }
     }
 
+    public void turnLeft() {
+        switch (direction) {
+            case "NORTH":
+                direction = "WEST";
+                break;
+            case "WEST":
+                direction = "SOUTH";
+                break;
+            case "SOUTH":
+                direction = "EAST";
+                break;
+            case "EAST":
+                direction = "NORTH";
+                break;
+        }
+    }
+
 
