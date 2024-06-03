d97ad52264ccabc0ed2193e5d6acf9b76e05b9d4
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-06-03 18:52:10 +0200

    Constructor del robot

--- Robot2/Robot.java
+++ Robot2/Robot.java
@@ -4,3 +4,11 @@ public class Robot {
     private int x; 
     private int y; 
     private String direction; 
+
+    public Robot(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+
