3f7a567b02eec9d04137778c71bc675ca7e5a888
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-06-03 18:55:51 +0200

    Método para cargar el estado del robot desde un archivo

--- Robot2/RobotController.java
+++ Robot2/RobotController.java
@@ -32,3 +32,16 @@ public class RobotController {
             e.printStackTrace();
         }
     }
+
+    public void loadRobotState() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("robot_state.txt"))) {
+            String[] state = reader.readLine().split(",");
+            int x = Integer.parseInt(state[0]);
+            int y = Integer.parseInt(state[1]);
+            String direction = state[2];
+            robot = new Robot(x, y, direction);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
